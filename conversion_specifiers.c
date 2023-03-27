f9d9bccf4b87d0ad442ca2f4dbe9f15448549f59
solomonha <hsol37@gmail.com>
2023-03-27 13:32:46 -0700

    adding lower and upper hexadecimal

--- conversion_specifiers.c
+++ conversion_specifiers.c
@@ -124,3 +124,89 @@ int print_S(va_list ap)
 	}
 	return (count - 1);
 }
+
+/**
+ * print_hexadecimalX - prints hexadecimal value of integers
+ *
+ * @ap: argument pointer to next argument, interger to be printed
+ *
+ * Return: character count from this function
+ */
+
+int print_hexadecimalX(va_list ap)
+{
+	int i = 0, remain = 0, save_digits = 0;
+	char *array = malloc(INT_MAX);
+	unsigned int n = va_arg(ap, int);
+
+	if (array == NULL)
+	{
+		return (0);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	while (n > 0)
+	{
+		remain = n % 16;
+		if (remain > 9)
+			array[i] = (remain - 10) + 'A';
+		else
+			array[i] = remain + '0';
+		n /= 16;
+		i++;
+	}
+	array[i] = '\0';
+	save_digits = (i - 1);
+	for (i = (i - 1); i >= 0; i--)
+	{
+		_putchar(array[i]);
+	}
+	free(array);
+	return (save_digits - 1);
+}
+
+/**
+ * print_hexadecimal - prints hexidecimal value of integers
+ *
+ * @ap: argument pointer to next argument, interger to be printed
+ *
+ * Return: character count from this function
+ */
+
+int print_hexadecimalx(va_list ap)
+{
+	int i = 0, remain = 0, save_digits = 0;
+	char *array = malloc(INT_MAX);
+	unsigned int n = va_arg(ap, int);
+
+	if (array == NULL)
+	{
+		return (0);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	while (n > 0)
+	{
+		remain = n % 16;
+		if (remain > 9)
+			array[i] = (remain - 10) + 'a';
+		else
+			array[i] = remain + '0';
+		n /= 16;
+		i++;
+	}
+	array[i] = '\0';
+	save_digits = (i - 1);
+	for (i = (i - 1); i >= 0; i--)
+	{
+		_putchar(array[i]);
+	}
+	free(array);
+	return (save_digits - 1);
+}
